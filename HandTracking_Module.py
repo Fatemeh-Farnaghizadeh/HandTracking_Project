e3cdf6a4da2c300fed1320c778cfa8796979cc02
fatemeh <fatemeh@gmail.com>
2023-06-29 15:19:24 +0330

    add py

---
+++ HandTracking_Module.py
@@ -0,0 +1,81 @@
+import cv2
+import mediapipe as mp
+import time
+
+class HandTracking():
+    
+    def __init__(self, static_image_mode=False, max_num_hands=2, min_detection_confidence=0.5, min_tracking_confidence=0.5):
+
+        self.mode = static_image_mode
+        self.num_hands = max_num_hands
+        self.detection_conf = min_detection_confidence
+        self.tracking_conf = min_tracking_confidence
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(static_image_mode=False, max_num_hands=2, min_detection_confidence=0.5, min_tracking_confidence=0.5)
+        self.mpDraw = mp.solutions.drawing_utils
+
+    def findHands(self, img, draw=True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
+        
+        if self.results.multi_hand_landmarks:
+
+            for handLms in self.results.multi_hand_landmarks:
+
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handLms,
+                                               self.mpHands.HAND_CONNECTIONS)
+                    
+        return img
+    
+    def findPosition(self, img, hand_num=0, draw=True):
+
+        lmList = []
+        if self.results.multi_hand_landmarks:
+            handLms = self.results.multi_hand_landmarks[hand_num]
+
+            for idx, lm in enumerate(handLms.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                lmList.append([idx, cx, cy])
+
+                if draw:
+                    
+                    cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
+
+        return lmList
+    
+
+def main():
+    pTime = 0
+    cTime = 0
+    cap = cv2.VideoCapture(0)
+    detector = HandTracking()
+
+    while True:
+        success, img = cap.read()
+        img = detector.findHands(img, False)
+        lmList = detector.findPosition(img)
+        if len(lmList) != 0:
+            print(lmList[4])
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3,
+                    (255, 0, 255), 3)
+        cv2.imshow("Image", img)
+        
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
+
+
+
+        
+       
+         
